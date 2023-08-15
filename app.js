e394af4b3918acfda848ac9599243a24e7862277
5Akan <akanimohsobot@gmail.com>
2023-08-15 18:10:39 +0100

    Testing window and global methods

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Testing");
+var time = 0;
+var timer = setInterval(function (params) {
+    time +=2;
+    console.log(time + ' seconds have passed');
+    if(time>5){
+        clearInterval(timer);
+    }
+},2000);
+
+console.log(__dirname)
+console.log(__filename)
