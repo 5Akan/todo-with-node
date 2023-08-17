751869e33b3cb492a65bbeb7a66bf6b5f0d2a020
5Akan <akanimohsobot@gmail.com>
2023-08-17 18:15:49 +0100

    Module pattern

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-//Using Require
-var counter = require('./count');
-console.log(counter(['Akanimoh', 'Rabi', 'Itoro', 'Uwem']));
+//Using Require to export multiple things
+var stuff = require('./stuff');
+console.log(stuff.counter(['Akanimoh', 'Rabi', 'Itoro', 'Uwem']));
+console.log(stuff.adder(2,4));
+console.log(stuff.adder(stuff.pi,4));
--- count.js
+++
@@ -1,8 +0,0 @@
-//countjs is a module
-var counter = function (arr) {
-
-    return 'There are ' + arr.length + ' elements in this array';
-    
-}
-
-module.exports = counter;
