020e335d51df360cba72d8eda26e16399c8988f9
5Akan <akanimohsobot@gmail.com>
2023-08-18 20:13:15 +0100

    Utility core module for inheriting other module functions

--- app.js
+++ app.js
@@ -4,13 +4,37 @@
 // console.log(stuff.adder(2,4));
 // console.log(stuff.adder(stuff.pi,4));
 
-//Node Event Emiiter
+//Node Event Emitter
     //It is a core module so we dont use a path but its name to call it
+    // var events = require('events');
+
+    // var emmitter = new events.EventEmitter();
+
+    // emmitter.on('someEvent', function (mssg) {
+    //     console.log(mssg)
+    // })
+
+    // emmitter.emit('someEvent','Emmited an Event')
     var events = require('events');
+    var util = require('util');// Used for inheriting things
 
-    var myEmmitter = new events.EventEmitter();
+    var Person = function(name) {
+        this.name = name;
+    }
 
-    myEmmitter.on('someEvent',function (mssg) {
-        console.log(mssg);
+    util.inherits(Person,events.EventEmitter);
+
+    var Akan = new Person('Akan');
+    var Rabi = new Person('Rabi');
+    var Uwem = new Person('Uwem');
+
+    var People = [Akan, Rabi, Uwem];
+
+    People.forEach(function (person) {
+        person.on("talk",function (num) {
+            console.log(`${person.name} said ${num}`)
+        })
+       
     })
-    myEmmitter.emit('someEvent', 'This event was emitted');
+
+    Akan.emit("talk", 9)
