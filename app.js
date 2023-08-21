c5b0c588d31600941c01bd2d12110fbd9f82e659
5Akan <akanimohsobot@gmail.com>
2023-08-21 18:51:02 +0100

    Reading and Writing Synchronous and Asynchronous files

--- app.js
+++ app.js
@@ -15,26 +15,43 @@
     // })
 
     // emmitter.emit('someEvent','Emmited an Event')
-    var events = require('events');
-    var util = require('util');// Used for inheriting things
+    // var events = require('events');
+    // var util = require('util');// Used for inheriting things
 
-    var Person = function(name) {
-        this.name = name;
-    }
+    // var Person = function(name) {
+    //     this.name = name;
+    // }
 
-    util.inherits(Person,events.EventEmitter);
+    // util.inherits(Person,events.EventEmitter);
 
-    var Akan = new Person('Akan');
-    var Rabi = new Person('Rabi');
-    var Uwem = new Person('Uwem');
+    // var Akan = new Person('Akan');
+    // var Rabi = new Person('Rabi');
+    // var Uwem = new Person('Uwem');
 
-    var People = [Akan, Rabi, Uwem];
+    // var People = [Akan, Rabi, Uwem];
 
-    People.forEach(function (person) {
-        person.on("talk",function (num) {
-            console.log(`${person.name} said ${num}`)
-        })
+    // People.forEach(function (person) {
+    //     person.on("talk",function (num) {
+    //         console.log(`${person.name} said ${num}`)
+    //     })
        
-    })
+    // })
 
-    Akan.emit("talk", 9)
+    // Akan.emit("talk", 9)
+
+//Reading and Writing Files
+
+//Synchronous method, ie executing one after the other
+
+// var fs = require('fs');
+// var readMe = fs.readFileSync('readMe.txt', 'utf8');
+// console.log(readMe);
+// fs.writeFileSync('writeMe.txt',readMe);
+
+//Asynchronous method
+var fs = require('fs');
+var readMe = fs.readFile('readMe.txt', 'utf8', function(err,data){
+    fs.writeFile('writeMe.txt',data,function(err,data){
+        
+    } );
+});
---
+++ readMe.txt
@@ -0,0 +1,1 @@
+Yay, Ak read me !!!
