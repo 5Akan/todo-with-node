5106db0752c694bc1b51aeebae0647482a8555af
5Akan <akanimohsobot@gmail.com>
2023-08-25 19:38:17 +0100

    Creating a Server

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 //Video 12
 //Creating a server 
-var http = require('http[]');
+var http = require('http');
+
+var server = http.createServer(function (req,res) {//A way to deal with request is using a function
+    console.log('Request was made: ' + req.url);
+    //Response Headers syntax
+    // res.writeHead(status,content-type)
+    res.writeHead(200,{'Content-Type':'text/plain'})
+    //Now we end the response and send it to the browser
+    res.end('Hey People');
+    
+})
+//We need to specify a port to listen to for request
+server.listen(3000,'127.0.0.1')
+console.log('Now listening to port 3000')
