5a87e5c373dbc63ac5baaef2ff333b38e7d92ead
5Akan <akanimohsobot@gmail.com>
2023-09-05 09:00:50 +0100

    Writing Streams

--- app.js
+++ app.js
@@ -16,15 +16,30 @@
 // console.log('Now listening to port 3000')
 
 //Video 14
+// var http = require('http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
+
+// //createReadStream has an eventlistener to listen to when the buffer sends a chunk of data
+// //Below it listens and fires the function when it receives a chunk of data
+// myReadStream.on('data',function (chunk) {
+//     console.log('New Chunk received')
+//     console.log(chunk) 
+
+// })
+
+//Video 14
 var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 //createReadStream has an eventlistener to listen to when the buffer sends a chunk of data
 //Below it listens and fires the function when it receives a chunk of data
 myReadStream.on('data',function (chunk) {
     console.log('New Chunk received')
-    console.log(chunk) 
+    myWriteStream.write(chunk);
 
 })
