8964c96a6a843f08caddd7d97845a16ecf33b212
5Akan <akanimohsobot@gmail.com>
2023-09-05 10:10:30 +0100

    Pipes for listening and writing the data/used it to send data to client side

--- app.js
+++ app.js
@@ -29,17 +29,38 @@
 
 // })
 
-//Video 14
+//Video 15
+// var http = require('http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+// //createReadStream has an eventlistener to listen to when the buffer sends a chunk of data
+// //Below it listens and fires the function when it receives a chunk of data
+// myReadStream.on('data',function (chunk) {
+//     console.log('New Chunk received')
+//     myWriteStream.write(chunk);
+
+// }
+
+//Video 16
+//Pipe..short of what is up
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+var server = http.createServer(function (req,res) {//A way to deal with request is using a function
+    console.log('Request was made: ' + req.url);
+    //Response Headers syntax
+    // res.writeHead(status,content-type)
+    res.writeHead(200,{'Content-Type':'text/plain'})
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
 
-//createReadStream has an eventlistener to listen to when the buffer sends a chunk of data
-//Below it listens and fires the function when it receives a chunk of data
-myReadStream.on('data',function (chunk) {
-    console.log('New Chunk received')
-    myWriteStream.write(chunk);
-
+    myReadStream.pipe(res);
+    
 })
+//We need to specify a port to listen to for request
+server.listen(3000,'127.0.0.1')
+console.log('Now listening to port 3000')
+
+
