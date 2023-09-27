494e5ca1c6c4b118d5a901cc73ca5e95b5db0f07
5Akan <akanimohsobot@gmail.com>
2023-09-27 18:58:12 +0100

    Middleware for static files

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs')
+app.use('/assets',express.static('assets'))
 
 app.get('/',function (req,res) {
     res.render('partials/use')
