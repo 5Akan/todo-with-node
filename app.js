faf2e8cefd887e4aba862ce24b49c8c22e1821a4
5Akan <akanimohsobot@gmail.com>
2023-10-12 13:39:32 +0100

    The second part of the bckend todo list

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var todoController = require('./controllers/todocontroller')
+var todoController = require('./controllers/todocontroller');
 
 var app = express();
 
@@ -11,6 +11,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 //The above isnt route specific ,ie  all routes are mapped to it
 //express.static() is a function built into express to serve up static files
+
+//Fire Controllers
+//By passing app into the funtion below it is available
+//to us in todocontroller.js
+todoController(app);
+
 
 //Listen to port
 app.listen(3000);
